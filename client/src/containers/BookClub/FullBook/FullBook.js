7e81df6b4af5e12d664cf90a55f5fe1463c48ffd
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:22:54 -0700

    add FullBook container for viewing/editing a singular book

---
+++ client/src/containers/BookClub/FullBook/FullBook.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axiosBooks from '../../../axios-books';
+
+import Book from '../../../components/Books/Book/Book';
+import Modal from '../../../components/UI/Modal/Modal';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+class FullBook extends Component {
+  static displayName = "[Component FullBook]";
+  state = {
+    loadedBook: null,
+    viewingBook: true
+  }
+
+  componentDidMount() {
+    const isbn = this.props.match.params.isbn;
+    if(isbn) {
+      if(!this.state.loadedBook || this.state.loadedBook.isbn !== isbn) {
+        //attempt to get book details from server
+        axiosBooks.get('/' + this.props.match.params.isbn)
+        .then(response => {
+          const book = response.data.data;
+          this.setState({loadedBook: book});
+        });
+      }
+    }
+  }
+  closeView = () => {
+    this.props.history.replace('/');
+  }
+  render() {
+    let book = <h2>No Book Found {":("}</h2>;
+    if(!this.state.loadedBook) {
+      book = <Spinner />
+    } else {
+      book = <Book img_url={this.state.loadedBook.img_url} />
+    }
+    return (
+        <Modal show={this.state.viewingBook} modalClosed={this.closeView}>
+            {book}
+        </Modal>
+    )
+  }
+}
+
+FullBook.propTypes = {};
+
+export default FullBook;
