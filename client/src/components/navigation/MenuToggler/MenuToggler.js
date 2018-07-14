e5ff64219605ad88b1abd942953fb77b3267723f
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:56:48 -0700

    Toolbal colouring adjusted and mobile only view menu is now a hamburger

--- client/src/components/navigation/MenuToggler/MenuToggler.js
+++ client/src/components/navigation/MenuToggler/MenuToggler.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import classes from './MenuToggler.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Aux from  '../../../hoc/auxillary/auxillary';
 const MenuToggler = (props) => (
   <Aux>
-    <div className={classes.MobileOnly} onClick={props.open}></div>
+    <div className={classes.MobileOnly} onClick={props.open}>
+      <FontAwesomeIcon icon={faBars} color="inherit"/>
+    </div>
   </Aux>
 );
 
