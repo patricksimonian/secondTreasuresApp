5787bddddb90379ef2f0f44411ee9e1b3bb8d495
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:47:46 -0700

    update logo with treasue map icon

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Logo.css';
 
-// import burgerLogo from '../../assets/images/burger_logo.png';
+import treasureLogo from '../../assets/images/treasuremap.png';
 
 const Logo = (props) => (
   <div className={classes.Logo} style={{height: props.height}}>
-    <img src="" alt="Burger Builder"/>
+    <img src={treasureLogo} alt="Second Treasures"/>
+    <span className={classes.caption}>Second Treasures</span>
   </div>
 );
 
