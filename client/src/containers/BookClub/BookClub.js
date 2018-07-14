6f929a282ce90ada81cb35fe8d3f6119b8c24222
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:34:37 -0700

    Book Club now loads books from api

--- client/src/containers/BookClub/BookClub.js
+++ client/src/containers/BookClub/BookClub.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
+import axiosBooks from '../../axios-books';
 
 import PropTypes from 'prop-types';
 import dummyBooks from '../dummy.json';
 //components
 import Books from '../../components/Books/Books';
 import FullBook from './FullBook/FullBook';
-
+import Spinner from '../../components/UI/Spinner/Spinner';
 class BookClub extends Component {
   static displayName = "";
   state = {
-    books: dummyBooks
+    books: [],
+    loadingBooks: true
   }
 
   componentDidMount() {
-    console.log(this.state.books);
+    axiosBooks.get('/')
+    .then(response => {
+      //books from data
+      this.setState({books: response.data.data, loadingBooks: false});
+    });
   }
   //book is clicked to view more details
   bookClickedHandler = (isbn) => {
@@ -23,9 +29,15 @@ class BookClub extends Component {
   }
 
   render() {
+    let books = null;
+    if(this.state.loadingBooks) {
+      books = <Spinner>Loading</Spinner>;
+    } else {
+      books = <Books books={this.state.books} viewBook={this.bookClickedHandler}/>;
+    }
     return (
       <div>
-        <Books books={this.state.books} viewBook={this.bookClickedHandler}/>
+        {books}
         <Route path='/books/:isbn' component={FullBook} />
       </div>
     )
