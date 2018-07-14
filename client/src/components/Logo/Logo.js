db407d9fd1b73547d542a54fc74742a0927029a8
patricksimonian <patricksimonian@gmail.com>
2018-07-14 10:34:14 -0700

    index.js now wrapped with browserrouter to get base react router route working

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './Logo.css';
 
-import burgerLogo from '../../assets/images/burger_logo.png';
+// import burgerLogo from '../../assets/images/burger_logo.png';
 
 const Logo = (props) => (
   <div className={classes.Logo} style={{height: props.height}}>
-    <img src={burgerLogo} alt="Burger Builder"/>
+    <img src="" alt="Burger Builder"/>
   </div>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+ReactDOM.render(root, document.getElementById('root'));
 registerServiceWorker();
