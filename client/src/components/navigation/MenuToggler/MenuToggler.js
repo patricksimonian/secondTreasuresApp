962dd8f8c43dc707a82989ae7811710241a33775
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:48:01 -0700

    navigation links set to home and login

--- client/src/components/navigation/MenuToggler/MenuToggler.js
+++ client/src/components/navigation/MenuToggler/MenuToggler.js
@@ -3,8 +3,7 @@ import classes from './MenuToggler.css';
 import Aux from  '../../../hoc/auxillary/auxillary';
 const MenuToggler = (props) => (
   <Aux>
-    <div className={classes.MobileOnly} onClick={props.open}>MENU2</div>
-    <div className={classes.DesktopOnly}>MENU</div>
+    <div className={classes.MobileOnly} onClick={props.open}></div>
   </Aux>
 );
 
