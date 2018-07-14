3f008786a7bd4ee42b4a59a7e6957a4bcccbf6bc
patricksimonian <patricksimonian@gmail.com>
2018-07-14 13:21:33 -0700

    change component name from book details to basic description as that name is reservered for another component

--- client/src/components/Books/Book/BasicDescription/BasicDescription.js
+++ client/src/components/Books/Book/BasicDescription/BasicDescription.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import classes from './BookDetail.css';
+import classes from './BasicDescription.css';
 import Authors from '../Authors/Authors.js';
-const BookDetail = (props) => (
-  <div className={classes.BookDetail}>
+const BasicDescription = (props) => (
+  <div className={classes.BasicDescription} onClick={props.clicked}>
     <h2 className={classes.title}>{props.title}</h2>
     <div className={classes.author}><Authors authors={props.authors}/></div>
   </div>
 );
 
-export default BookDetail;
+export default BasicDescription;
