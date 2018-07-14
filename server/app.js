fa261a4171d257248f10c0ccaa6dc1a2a2e0cb94
patricksimonian <patricksimonian@gmail.com>
2018-07-13 19:18:45 -0700

    move package json up directory

--- server/app.js
+++ server/app.js
@@ -3,25 +3,28 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cloudinary = require('cloudinary');
+const chalk = require('chalk');
 //get config by environment
 const config = require('./config/config.js')();
 const port = config.PORT || 3000;
 //initiate server
 const app = express();
 //get database instance
-const db = require('./db/db.js');
+const db = require('./db/db.js')(config);
 //set cloudinary config
 cloudinary.config(config.cloudinary);
 // routers
 const bookRoutes = require('./routes/books.js');
 const authRoutes = require('./routes/auth.js');
 // middlewares
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../', '/client/build')));
 // parse json data
 app.use(bodyParser.json());
 //ensure content type is always application/json
-app.use((req, res, next) => {
+app.use('/api', (req, res, next) => {
   if(!req.is('application/json')) {
-    res.send(415);
+    res.sendStatus(415);
   } else {
     next();
   }
@@ -30,8 +33,13 @@ app.use((req, res, next) => {
 app.use('/api/books', bookRoutes(db, cloudinary, config.jwt.secret));
 app.use('/api', authRoutes(db, config.jwt.secret));
 
+//main end point for react app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
+
 db.sequelize.sync().then(() => {
   app.listen(config.port, () => {
     console.log('Express listening on port:', config.port);
   });
-});
+}).catch(err => console.log(err));
--- server/db/db.js
+++ server/db/db.js
@@ -1,31 +1,33 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.js')();
-const sequelize = new Sequelize(config.database, config.db_user, config.db_password, {
-  host: config.db_host,
-	dialect: config.dialect,
-  operatorsAliases: false,
-  define: {
-    underscored: true
-  }
-});
-//connect all models/tables to a singular db object for ease of access
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+const dbInstance = function(config) {
+  const sequelize = new Sequelize(config.database, config.db_user, config.db_password, {
+    host: config.db_host,
+  	dialect: config.dialect,
+    operatorsAliases: false,
+    define: {
+      underscored: true
+    }
+  });
+  //connect all models/tables to a singular db object for ease of access
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
 
 
-//connect all models
-db.admins = require('../models/admin.js')(sequelize, Sequelize);
-db.authors = require('../models/author.js')(sequelize, Sequelize);
-db.books = require('../models/book.js')(sequelize, Sequelize);
-db.employees = require('../models/employee.js')(sequelize, Sequelize);
-db.ratings = require('../models/rating.js')(sequelize, Sequelize);
-//setup associations
-db.books.hasMany(db.ratings);
-db.ratings.belongsTo(db.books, {foreignKey: 'book_isbn'});
-db.employees.hasOne(db.admins);
+  //connect all models
+  db.admins = require('../models/admin.js')(sequelize, Sequelize);
+  db.authors = require('../models/author.js')(sequelize, Sequelize);
+  db.books = require('../models/book.js')(sequelize, Sequelize);
+  db.employees = require('../models/employee.js')(sequelize, Sequelize);
+  db.ratings = require('../models/rating.js')(sequelize, Sequelize);
+  //setup associations
+  db.books.hasMany(db.ratings);
+  db.ratings.belongsTo(db.books, {foreignKey: 'book_isbn'});
+  db.employees.hasOne(db.admins);
 
-db.books.belongsToMany(db.authors, {as:'authors', through: 'book_authors', foreignKey: 'book_isbn'});
-db.authors.belongsToMany(db.books, {as:'books', through: 'book_authors', foreignKey: 'author_id'});
+  db.books.belongsToMany(db.authors, {as:'authors', through: 'book_authors', foreignKey: 'book_isbn'});
+  db.authors.belongsToMany(db.books, {as:'books', through: 'book_authors', foreignKey: 'author_id'});
+  return db;
+}
 
-module.exports = db;
+module.exports = dbInstance;
