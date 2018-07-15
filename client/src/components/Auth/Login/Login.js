5f32ca907397d3ed4bceb80ee2ad952766cd3285
patricksimonian <patricksimonian@gmail.com>
2018-07-15 10:55:34 -0700

    include message list into login compoennt

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -2,9 +2,11 @@ import React from 'react';
 import classes from './Login.css';
 import Input from '../../UI/Input/Input';
 import Button from '../../UI/Button/Button';
+import MessageList from '../../UI/MessageList/MessageList';
 const Login = (props) => (
   <div className={classes.Login}>
     <p>Employee Login Portal</p>
+    <MessageList messages={props.messages} />
     <form onSubmit={props.login} >
       <Input
         type="text"
