61f5c822a2b4d22750cc1968f90f8ff8b7e128ef
patricksimonian <patricksimonian@gmail.com>
2018-07-14 17:53:48 -0700

    nav links now leverage react router

--- client/src/components/navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ client/src/components/navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import {NavLink} from 'react-router-dom';
 
 const NavigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a
-      className={props.active ? classes.active : null}
-      href={props.link}>{props.children}</a>
+    <NavLink activeClassName={classes.active}
+      to={props.link} exact>{props.children}</NavLink>
   </li>
 );
 
--- client/src/components/navigation/NavigationItems/NavigationItems.js
+++ client/src/components/navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = (props) => (
  <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>home</NavigationItem>
+    <NavigationItem link="/">home</NavigationItem>
     <NavigationItem link="/login">employee login</NavigationItem>
  </ul>
 );
