a028443162966bdd1cf1ce66ce622c5c3e5cf30b
patricksimonian <patricksimonian@gmail.com>
2018-07-15 15:32:03 -0700

    revamp Input component to allow for dynamic input generation via config, this also includes validations

--- client/src/components/UI/Input/Input.js
+++ client/src/components/UI/Input/Input.js
@@ -1,29 +1,59 @@
 import React from 'react';
+
 import classes from './Input.css';
 
-const Input = (props) => {
-  let inputElement = null;
-  switch (props.inputtype) {
-    case 'input':
-      inputElement = <input className={classes.InputElement} {...props}/>
-    break;
-    case 'select':
-      inputElement = <select className={classes.InputElement} {...props}/>
-    break;
-    case 'textarea':
-      inputElement = <textarea className={classes.InputElement} {...props}/>
-    break;
-    default:
-      inputElement = <input className={classes.InputElement} {...props}/>
-    break;
-  }
+const input = ( props ) => {
+    let inputElement = null;
+    const inputClasses = [classes.InputElement];
 
-  return (
-    <div className={classes.Input}>
-      <label className={classes.Label}>{props.label}</label>
-      {inputElement}
-    </div>
-  );
-}
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
 
-export default Input;
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+
+};
+
+export default input;
