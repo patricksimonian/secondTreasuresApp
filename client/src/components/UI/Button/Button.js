e18637df3c83c695038864e4fe8a332b3d7c23ef
patricksimonian <patricksimonian@gmail.com>
2018-07-15 10:33:26 -0700

    added prop to enable/disable ui button

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import classes from './Button.css';
-
+import PropTypes from 'prop-types'
 const button = (props) => (
-  <button onClick={props.clicked} className={[classes.Button, classes[props.buttonType]].join(" ")}>{props.children}</button>
+  <button
+    disabled={!props.enabled}
+    onClick={props.clicked}
+    className={[classes.Button, classes[props.buttonType]].join(" ")}>{props.children}</button>
 );
 
+button.displayName = '[Component Button]';
+button.PropTypes = {}
 export default button;
