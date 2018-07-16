d9d7130fd3dad8fc63945f3a24247eb0a488d9be
patricksimonian <patricksimonian@gmail.com>
2018-07-15 17:53:59 -0700

    set get all books method to use raw query as demonstration, also added some data massaging to price to so that its' saved as cents

--- server/routeHandlers/bookHandlers.js
+++ server/routeHandlers/bookHandlers.js
@@ -6,20 +6,45 @@ module.exports = (db, cloudinary) => {
   const Author = db.authors;
   return  {
     allBook: (req, res) => {
-       Book.all({
-         include: [{model: Author, as: 'authors'}]
-       })
+      //pagnation query params if passed in
+      let page = req.query.page / 1
+      const NUMROWS = 25;
+      page = isNaN(page) || page < 1 ? 1 : Math.floor(page); //is page param passed in as a valid int ?
+      const startRow = page === 1 ? 1 : page * NUMROWS;
+      const endRow = startRow + NUMROWS;
+      // using raw query for now as demonstration of sql
+      // please note that although RAW values are being tossesd into the LIMIT
+      // there is no possibility of inject due to the validations above
+      const query = `
+        SELECT b.isbn, b.title, b.genre, cast((b.price / 100) as decimal(11, 2)) AS price, b.price as price_cents,
+        b.stock, b.img_url, a.name AS author_name, a.id AS author_id
+        FROM Books b
+        LEFT JOIN book_authors ba
+        ON b.isbn = ba.book_isbn
+        LEFT JOIN Authors a
+        ON ba.author_id = a.id
+        ORDER BY b.created_at DESC
+        LIMIT ${startRow}, ${endRow};`
+
+      db.sequelize.query(query, {bind: [startRow, endRow], type: db.sequelize.QueryTypes.SELECT})
        .then(books => {
-         //map books to json
+         //format book data in a more predicatble manner, storing authors under
+         //books
          const booksJSON = books.map(book => {
-           return {...book.toJSON(), isbn_formatted: book.isbn_formatted()}});
+           return {
+            ...book,
+            authors: [{name: book.author_name, id: book.author_id}],
+            isbn_formatted: beautifyIsbn.hyphenate(book.isbn)}
+         });
+         //send book data
          res.json({
            success: true,
            data: booksJSON,
-           message: ['Books found']
+           message: [books.length ? 'Books found' : 'No Books Found']
          });
        })
        .catch(err => {
+         console.log(err);
          //failed to load books
          //very unlikely but we should notify admins that something is wrong with website
          res.status(500).json({
@@ -70,6 +95,9 @@ module.exports = (db, cloudinary) => {
         });
       })
     },
+    //create books for now only allows to store on author by their name,
+    //this will remain this way until the front end allows for dynamic generation
+    //of author inputs
     createBook: (req, res) => {
       //get data from request body
       const requestData = req.body;
@@ -77,21 +105,26 @@ module.exports = (db, cloudinary) => {
       cloudinary.uploader.upload(requestData.book.img_url)
       .then(result => {
         let book = {...requestData.book};
-        let author = {...requestData.author};
+        let author = requestData.book.author; // <SUBJECT TO CHANGE to be more dynamic
         //update book img url with the secure version from hosting
         book.img_url = result.secure_url;
+        //format book price to cents
+        book.price = book.price * 100;
         //attempt to create the book
+        //ideally we should place this in a transaction**
         return Book.create(book)
         .then(newBook => {
           //find or create author for book
-          return Author.findById(author.id)
+          //please note for now we are only handdling the case of a singular author
+          //this would be a promise.All in the case we choose to add more authors
+          return Author.findOne({where: {name: author}})
           .then(authorFound => {
             if(authorFound) {
               return newBook.setAuthors(authorFound);
             } else {
               //if author wasn't found we reset id to null and create author
               author.id = null;
-              return Author.create(author)
+              return Author.create({name: author})
               .then(authorCreated => newBook.setAuthors(authorCreated));
             }
           });
